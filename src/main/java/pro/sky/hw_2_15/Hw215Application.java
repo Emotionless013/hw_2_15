0d6dc2e4124203be5910e5d16ab952b04f5cd92f
Alexander <emotionless013@mail.ru>
2022-10-24 12:25:59 +0400

    1st commit

---
+++ src/main/java/pro/sky/hw_2_15/Hw215Application.java
@@ -0,0 +1,13 @@
+package pro.sky.hw_2_15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw215Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw215Application.class, args);
+    }
+
+}
